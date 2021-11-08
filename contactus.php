b3a530072831983df3da5b2b17df7d335c0c0feb
Aminnur00 <93878747+Aminnur00@users.noreply.github.com>
2021-11-08 09:47:27 +0800

    Create contactus.php

---
+++ contactus.php
@@ -0,0 +1,9 @@
+<?php
+$name = $_GET['name'];
+$email = $_GET['email'];
+$phone = $_GET['phone'];
+$subject = $_GET['subject'];
+$message = $_GET['message'];
+
+echo $name, '/' , $email, '/' , $phone, '/' , $subject, '/' , $message;
+?>
